7cf251f7ded451ebe4c3d11104443ab24bfc1853
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-10-18 09:29:13 +0200

    Add files via upload

---
+++ TreciZadtak/TreciZadtak/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreciZadtak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Brod brod = new Brod();
+            Zrakoplov zrakoplov = new Zrakoplov();
+            brod.KudaVozi();
+            zrakoplov.KudaVozi();
+        }
+    }
+}
---
+++ TreciZadtak/TreciZadtak/Zrakoplov.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreciZadtak
+{
+    class Zrakoplov : Vozilo
+    {
+        public Zrakoplov()
+        {
+            JeLiLeti = true; 
+            JeLiPlovi = false;
+        }
+}
