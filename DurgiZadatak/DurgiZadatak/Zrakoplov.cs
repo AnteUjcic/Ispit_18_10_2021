03791561e9171adb873594965660e4a01724eb7d
AnteUjcic <58471899+AnteUjcic@users.noreply.github.com>
2021-10-18 09:08:51 +0200

    Add files via upload

---
+++ DurgiZadatak/DurgiZadatak/Zrakoplov.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DurgiZadatak
+{
+    class Zrakoplov
+    {
+        private string naziv;
+        private double snagaMotora;
+        private int doesgLeta;
+
+        public Zrakoplov(string naziv, double snagaMotora, int doesgLeta)
+        {
+            this.naziv = naziv;
+            this.snagaMotora = snagaMotora;
+            this.doesgLeta = doesgLeta;
+        }
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public double SnagaMotora { get => snagaMotora; set => snagaMotora = value; }
+        public int DoesgLeta { get => doesgLeta; set => doesgLeta = value; }
+    }
+}
